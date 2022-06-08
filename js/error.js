b2372eb9e34406b71e40e6dfa106f57553b94b16
fraancofenoglio <francofeno26@gmail.com>
2022-06-08 10:56:20 -0300

    4th-commit

--- js/error.js
+++ js/error.js
@@ -6,7 +6,7 @@ export function mostrarError(message) {
 
     div.appendChild(alerta);
     div.style = 'display:flex;justify-content:center;align-items:center;'
-    alerta.style = 'background:red;color:white;width:40%;text-align:center;border-radius:5px;';
+    alerta.style = 'background:red;color:white;width:100%;text-align:center;border-radius:5px;';
     alerta.innerText = message;
 
     container.appendChild(div);
